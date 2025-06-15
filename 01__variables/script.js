1a3d04c848139f135d589d43bb02e7bee52110b6
rahulvalmiki <129489633+rahulvalmiki@users.noreply.github.com>
2025-06-15 22:58:56 +0530

    Create script.js

---
+++ 01__variables/script.js
@@ -0,0 +1,51 @@
+// var , let , const
+// Q 1 -  var
+// note - var hota hai w
+//              ex - 01
+if (true) {
+    // var a = 10;
+    // console.log(a);
+
+}
+// console.log(a);
+
+// Q 02 - let
+// note - 
+//              ex - 01 
+if (true) {
+    let a = 10;
+    // console.log(a);
+
+}
+// console.log(a);
+
+// Q - 03 const
+
+if (true) {
+    const b = 10;
+    // console.log(b);
+
+}
+// console.log(b);
+
+// var : redeclare allowed
+// note - 
+var x = 5
+var x = 10
+
+// let : redeclare  not allowed
+let x = 5
+let x = 10
+
+// let : Reassign allowed
+let a = 5
+a = 10
+
+// const : redeclare  not allowed
+const z = 10
+const x = 10  // not allowed
+
+// const : Reassign not allowed
+
+const c = 10 
+c = 20  // not allowed
