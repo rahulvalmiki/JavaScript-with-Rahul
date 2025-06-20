11c82b6c66d53ffaac6062ffd7b74462b883dc2f
rahulvalmiki <129489633+rahulvalmiki@users.noreply.github.com>
2025-06-20 18:47:05 +0530

    Create index.js

---
+++ 02_datatype/index.js
@@ -0,0 +1,48 @@
+
+//Primitive Types
+// Q1 - number
+// Note - 
+
+let age = 18 // = number
+let num = 6395 // = number
+let per = 98.36 // = number
+//console.log(typeof age);
+
+
+// Q2 - string
+// Note - 
+// code - 
+//              ex - 01 
+let name = "rahul"// string
+let address = "15/963 sh varanasi"
+console.log(typeof name);
+
+
+// Q3 - boolean
+// Note - 
+// code - 
+//                  ex - 01 
+let isStudent = true // sach
+let isAdnmin  = false // jhut
+console.log(typeof isStudent);
+
+
+//Q4 - Undefined
+// Note - jab kabhi bina value ke koi bhi varible bnaya jata hai to wo Undefined ho jati hai 
+// code - 
+//                      ex - 01 
+let score ;
+console.log(score);
+
+let data = null // kuch nhi
+
+// Q5 - symbol
+let id = Symbol("id");
+console.log(typeof id);
+
+
+// Q7 - bigint
+// note - 
+// code - 
+//              ex - 01 
+let gibNumber = 1245782532585455444544454454788963115215415451255
